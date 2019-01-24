d5e2738ce9503aba6fea1b1f80a8aca8881aaddf
Ping <guillaume.pn@eduge.ch>
2019-01-24 11:02:37 +0100

    forgotten the header

--- RobotMove/RobotMove/PictureData.cs
+++ RobotMove/RobotMove/PictureData.cs
@@ -1,9 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/*
+ *  Author      : Guillaume Pin 
+ *  Date        : 24-01-2019
+ *  Project     : AnalyseRealTime
+ *  Version     : 1.0
+ *  File        : PictureData.cs
+ *  description : 
+ */
+
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RobotMove
 {
